6932e72755a168f45a3ecfbb970ee2a9b6d2e3b7
noelmclark <59598629+noelmclark@users.noreply.github.com>
2025-07-21 14:51:45 -0600

    adding another function from rCNV

--- workflow/scripts/rCNV-test.R
+++ workflow/scripts/rCNV-test.R
@@ -556,6 +556,17 @@ cpm.normal <- function(het.table, method=c("MedR","QN","pca","TMM","TMMex"),
 }
 
 
+makeTransparent = function(..., alpha=0.5) {
+  if(alpha<0 | alpha>1) stop("alpha must be between 0 and 1")
+  alpha = floor(255*alpha)
+  newColor = col2rgb(col=unlist(list(...)), alpha=FALSE)
+  .makeTransparent = function(col, alpha) {
+    rgb(red=col[1], green=col[2], blue=col[3], alpha=alpha, maxColorValue=255)
+  }
+  newColor = apply(newColor, 2, .makeTransparent, alpha=alpha)
+  return(newColor)
+}
+
 
 
 
