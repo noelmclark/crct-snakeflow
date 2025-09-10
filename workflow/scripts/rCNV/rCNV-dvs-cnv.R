e50f3926b6c1b5d418128a53e4b6e732e35a1c2a
noelmclark <59598629+noelmclark@users.noreply.github.com>
2025-09-10 12:52:00 -0600

    adding cleaning section to r script for interactive session

--- workflow/scripts/rCNV/rCNV-dvs-cnv.R
+++ workflow/scripts/rCNV/rCNV-dvs-cnv.R
@@ -58,3 +58,43 @@ for (vcf_file in vcf_files) { #replace vcf_files with leftover if there are file
 }
 
 cat("All VCF files processed.\n")
+
+
+## now process dvs and cnv tables to remove flagged variants and join together into one tsv
+
+# Define new directories
+dvs_clean_dir <- "./results/rCNV-by-scat/dvs-clean" 
+cnv_clean_dir <- "./results/rCNV-by-scat/cnv-clean" 
+
+# Create output directory if it doesn't exist
+if (!dir.exists(dvs_clean_dir)) {
+  dir.create(dvs_clean_dir)
+}
+if (!dir.exists(cnv_clean_dir)) {
+  dir.create(cnv_clean_dir)
+}
+
+dvs_files <- list.files(dvs_dir, pattern = "\\.tsv$", full.names = TRUE)
+cnv_files <- list.files(cnv_dir, pattern = "\\.tsv$", full.names = TRUE)
+
+for (dvs_file in dvs_files) {
+    cat("Cleaning:", dvs_file, "\n")
+
+    # Construct output file name
+    base_name <- tools::file_path_sans_ext(basename(dvs_file))
+    dvs_clean <- file.path(dvs_clean_dir, paste0(base_name, "_dvs_clean.tsv"))
+    
+    # Load the table
+    dvs <- read_tsv(dvs_files)
+    
+    # Filter dvs files by removing rows with "deviant" flag
+    dvs_cleaned <- dvs %>% filter(dup.stat == "non-deviant")
+    
+    # write new file 
+    write_tsv(dvs_cleaned, file = dvs_clean)
+
+    # Remove intermediate files from global environment before restarting
+    rm(dvs, dvs_cleaned)
+
+    cat("Finished cleaning and saved:", dvs_file, "\n")
+}
